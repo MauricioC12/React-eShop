69a25a0c59257b9596b5b2763a6b5df94ff056d1
mauricioc12 <mauriciocontrerasr1202@gmail.com>
2022-11-02 12:57:59 -0500

    Initial commit

---
+++ src/components/OrderItem.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import AppContext from '@context/AppContext';
+import '@styles/OrderItem.scss';
+import iconClose from '@icons/icon_close.png';
+
+const OrderItem = ({ product }) => {
+	const { removeFromCart } = useContext(AppContext);
+
+	return (
+		<div className="OrderItem">
+			<figure>
+				<img src={product.images[0]} alt={product.title} />
+			</figure>
+			<p>{product.title}</p>
+			<p>${product.price}</p>
+			<img
+				src={iconClose}
+				alt="close"
+				onClick={() => removeFromCart(product)}
+				className="removeImage"
+			/>
+		</div>
+	);
+}
+
+export default OrderItem;
